7f8b689a83b9bbf702bf435fd950bf6d5c9d8be7
ArshesSH <105101004ksh@gmail.com>
2022-04-22 23:54:26 +0900

    add wndclassex

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -6,6 +6,35 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow )
 {
-	while( true );
+	/*
+	* To make windows, need to
+	* 1) register window class
+	* 2) create window instance
+	*/
+
+	const auto pClassName = "hw3dThings";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof( wc );
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	/*
+	* RegisterClassEX register window classand it's a ex ver.
+	* It takes pointer of WNDCLASSEX
+	*/
+	RegisterClassEx( &wc );	
+
+	// create window instance
+
 	return 0;
 }
