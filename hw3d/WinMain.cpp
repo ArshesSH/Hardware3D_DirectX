98f4177662e5459fb02b7cf1b32d10d3272a3183
ArshesSH <105101004ksh@gmail.com>
2022-05-02 21:28:39 +0900

    T3 Implement Message Loop & WndProc

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -10,6 +10,17 @@
 
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	switch ( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage( 69 );
+		break;
+	}
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -30,7 +41,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc; // DefWindowProc -> WndProc
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -59,7 +70,22 @@ int CALLBACK WinMain(
 	// show the window
 	ShowWindow( hWnd, SW_SHOW );
 
-	while (true);
+	// Message pump
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0  )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
 
+	if ( gResult == -1 )
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 	return 0;
 }
